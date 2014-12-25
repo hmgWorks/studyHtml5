67e2854434694ec70b895a6e220edd213f779af9
hmg <tmdnldjazz@gmail.com>
2014-12-25 17:05:54 +0900

    add input system

--- GameFrameWork/GameFramework.js
+++ GameFrameWork/GameFramework.js
@@ -1,12 +1,31 @@
 window.addEventListener('load', onPageLoadComplete, false);
 
+//test
+var temp_text_x = 400;
+var temp_text_y = 300;
+
 function onPageLoadComplete () {
     var FPS = 30;
     setInterval(gameLoop, 1000 / FPS);
 }
 
 function Update () {
-
+    if (inputSystem.isKeyDown(37)) {
+        //left
+        temp_text_x -= 5;
+    }
+    if (inputSystem.isKeyDown(39)) {
+        //Right
+        temp_text_x += 5;
+    }
+    if (inputSystem.isKeyDown(38)){
+        //Top
+        temp_text_y -= 5;
+    }
+    if (inputSystem.isKeyDown(40)){
+        //Botton
+        temp_text_y += 5;
+    }
 }
 
 function Render () {
@@ -21,7 +40,11 @@ function Render () {
     Context.font =          '25px arial';
     Context.textBaseline =  'top';
     Context.fillText('fps :' + frameCounter.Lastfps, 10, 10);
-}
+
+//test
+    Context.font = '40px arial';
+    Context.fillText('★', temp_text_x, temp_text_y);
+}   
 
 function gameLoop () {
     Update();
